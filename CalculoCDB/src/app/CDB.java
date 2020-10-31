ebfb5dfc34cf79dbe0995b52dc387aec9e07b084
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2020-10-31 15:59:03 -0300

    5o. commit → obter barra verde após novo teste (3o. teste) escrito para a unidade. Triangular e parametrizar
    Co-authored-by: Danilo <danilow200@gmail.com>

--- CalculoCDB/src/app/CDB.java
+++ CalculoCDB/src/app/CDB.java
@@ -6,15 +6,16 @@ public class CDB {
 	private float aplicacaoInicial;
 	private float txJurosAnual;
 	private float dias;
+	private float totalRend;
 	
 	public void cadastrarAplicacao(float aplicacaoInicial, float txJurosAnual, int dias  ) {
 		this.aplicacaoInicial = aplicacaoInicial;
 		this.txJurosAnual = txJurosAnual;
 		this.dias = dias;
+		this.totalRend+=(this.aplicacaoInicial*this.txJurosAnual/100)*this.dias/365;
 
 	}
 	public float getRendBruto() {
-		float rend = (this.aplicacaoInicial*this.txJurosAnual/100)*this.dias/365;
-		return rend; //pela tecnica de falsificacao
+		return this.totalRend;
 	}
 }
--- CalculoCDB/src/tests/CalculoRendBruto.java
+++ CalculoCDB/src/tests/CalculoRendBruto.java
@@ -18,12 +18,19 @@ public class CalculoRendBruto {
 	@Test
 	public void testCalcularRendBruto() {
 		cdb.cadastrarAplicacao(1000f, 8.5f, 60);
-		assertEquals(13.97f, cdb.getRendBruto(), 0.009);
+		assertEquals(13.97f, cdb.getRendBruto(), 0.01);
 	}
 	
 	@Test
 	public void testCalcularRendBruto2() {
 		cdb.cadastrarAplicacao(500f, 8.0f, 120);
-		assertEquals(13.15f, cdb.getRendBruto(), 0.009);
+		assertEquals(13.15f, cdb.getRendBruto(), 0.01);
+	}
+	
+	@Test
+	public void testCalcularRendBruto3() {
+		cdb.cadastrarAplicacao(1000f, 8.5f, 60);
+		cdb.cadastrarAplicacao(500f, 8.0f, 120);
+		assertEquals(27.12f, cdb.getRendBruto(), 0.01);
 	}
 }
