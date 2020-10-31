6657fd177786bf3882f7f385a5c200dc153c830d
PauloVitorRocha <paulovitorrocha.unb@gmail.com>
2020-10-31 13:02:47 -0300

    1commit falha por falta de implementação da unidade

---
+++ CalculoCDB/src/tests/CalculoRendBruto.java
@@ -0,0 +1,22 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import app.CDB;
+
+public class CalculoRendBruto {
+	private CDB cdb;
+	
+	@Before
+	public void setup() {
+		cdb = new CDB();
+	}
+	
+	@Test
+	public void testCalcularRendBruto() {
+		fail("Unidade não implementada");
+	}
+}
