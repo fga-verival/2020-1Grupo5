98aabc008210a9966333e1da6a6b39c9cdf102c0
Elias Bernardo <ebmm01@gmail.com>
2020-11-01 16:46:58 -0300

    3o commit - Obter barra vermelha por novo teste para a mesma unidade - Rendimento Liquido
    Co-authored-by: Arthur Rodrigues <arthurarp97@gmail.com>
    Co-authored-by: Rafaella Junqueira <rafaellafjunqueira@gmail.com>
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- CalculoCDB/src/tests/CalculoRendimentoLiquido.java
+++ CalculoCDB/src/tests/CalculoRendimentoLiquido.java
@@ -20,4 +20,10 @@ public class CalculoRendimentoLiquido {
 		cdb.cadastrarAplicacao(1000f, 8.5f, 60);
 		assertEquals(1.08f, cdb.getRendimentoLiquido(), 0.01);
 	}
+	
+	@Test
+	public void testRendimentoLiquido2() {
+		cdb.cadastrarAplicacao(500f, 8.0f, 120);
+		assertEquals(2.03f, cdb.getRendimentoLiquido(), 0.01);
+	}
 }
